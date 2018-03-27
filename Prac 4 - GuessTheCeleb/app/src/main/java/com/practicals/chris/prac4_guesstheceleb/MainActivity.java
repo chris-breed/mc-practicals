35fe458ff4a39440617e7ab5fd980cb38ed6b863
chris-breed <christopher.breed@my.jcu.edu.au>
2018-03-27 22:25:04 +1000

    start of fragments

--- Prac 4 - GuessTheCeleb/app/src/main/java/com/practicals/chris/prac4_guesstheceleb/MainActivity.java
+++ Prac 4 - GuessTheCeleb/app/src/main/java/com/practicals/chris/prac4_guesstheceleb/MainActivity.java
@@ -1,13 +1,23 @@
 package com.practicals.chris.prac4_guesstheceleb;
 
+import android.app.Fragment;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener, SecondaryFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment base;
+        base = findViewById(R.id.fragment);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
