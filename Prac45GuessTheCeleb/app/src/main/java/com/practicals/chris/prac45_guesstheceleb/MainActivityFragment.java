f7b56a963c3780b03821025a32d85156d3ed5105
Christopher Breed <jc320960@ad.jcu.edu.au>
2018-04-04 13:53:48 +1000

    celeb guessing game

--- Prac45GuessTheCeleb/app/src/main/java/com/practicals/chris/prac45_guesstheceleb/MainActivityFragment.java
+++ Prac45GuessTheCeleb/app/src/main/java/com/practicals/chris/prac45_guesstheceleb/MainActivityFragment.java
@@ -6,10 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import java.util.Dictionary;
+
+
 public class MainActivityFragment extends Fragment {
+
 
     public MainActivityFragment() {
     }
@@ -18,5 +19,7 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+
+
     }
 }
